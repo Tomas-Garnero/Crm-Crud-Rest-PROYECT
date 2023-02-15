ab9769c125898932cfc117c007adec087e34d099
unknown <tomygarnero9429@gmail.com>
2023-02-15 19:11:15 -0300

    Save client changes to edit

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,4 +1,5 @@
-import { obtenerCliente } from "./API.js";
+import { obtenerCliente, editarCliente } from "./API.js";
+import { mostrarAlerta, validar } from "./funciones.js";
 
 (function() {
 
@@ -16,6 +17,10 @@ import { obtenerCliente } from "./API.js";
         const cliente = await obtenerCliente(idCliente);
 
         mostrarCliente(cliente);
+
+        // Submit al formulario
+        const formulario = document.querySelector("#formulario");
+        formulario.addEventListener("submit", validarCliente);
     });
 
     function mostrarCliente(cliente) {
@@ -26,5 +31,32 @@ import { obtenerCliente } from "./API.js";
         telefonoInput.value = telefono;
         emailInput.value = email;
         idInput.value = id;
+    }
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        
+        const nombre = document.querySelector("#nombre").value;
+        const email = document.querySelector("#email").value;
+        const telefono = document.querySelector("#telefono").value;
+        const empresa = document.querySelector("#empresa").value;
+
+        const cliente = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: parseInt(idInput.value)
+        }
+
+        if(validar(cliente)) {
+            // Mostrar mensaje
+            mostrarAlerta("Todos los campos son obligatorios");
+            return;
+        }
+
+        // Rescribe el objeto
+        editarCliente(cliente);
     }
 })();
